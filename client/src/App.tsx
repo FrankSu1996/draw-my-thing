ea2313e9addcb994b3d6af742d95152c069b4214
Frank Su <franksu49@gmail.com>
2024-04-15 19:03:04 -0400

    hook up shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { socket } from "./socket";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [isConnected, setIsConnected] = useState<boolean>(socket.connected);
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message: "hello" });
+  };
 
   useEffect(() => {
     function onConnect() {
@@ -15,15 +20,19 @@ function App() {
     }
 
     socket.on("connect", onConnect);
+    socket.on("receive_message", (data) => {
+      alert(data.message);
+    });
 
     return () => {
       socket.off("connect", onConnect);
       socket.off("disconnect", onDisconnect);
     };
   }, []);
+
   return (
     <>
-      <div>{isConnected.toString()}</div>
+      <Button onClick={sendMessage}>Test</Button>
     </>
   );
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -18,5 +18,9 @@ const io = new Server(port, {
 });
 
 io.on("connection", (socket) => {
-  console.log("connected!");
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", data);
+  });
 });
